ee2085f85e91e34654d68b76686d36c7e27db7b6
HarshTechnomark <harsh.dhaduk@technomark.io>
2023-11-23 18:16:46 +0530

    updated code

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -941,13 +941,13 @@ const page = () => {
         const blob = new Blob([response.data], {
           type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         });
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = `${filename}.xlsx`;
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
+        // const url = window.URL.createObjectURL(blob);
+        // const a = document.createElement("a");
+        // a.href = url;
+        // a.download = `${filename}.xlsx`;
+        // document.body.appendChild(a);
+        // a.click();
+        // window.URL.revokeObjectURL(url);
         setIsExporting(false);
         Toast.success("Data exported successfully.");
       } else {
