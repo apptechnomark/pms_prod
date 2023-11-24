47c64148b0b130158ce9c067210edbb11b2ba07f
HarshTechnomark <harsh.dhaduk@technomark.io>
2023-11-24 13:50:39 +0530

    updated code

--- src/utils/downloadFile.ts
+++ src/utils/downloadFile.ts
@@ -1,10 +1,9 @@
 import { BlobServiceClient } from "@azure/storage-blob";
 
 export const getFileFromBlob = async (fileName: string) => {
-  const storageAccount = "pmsdevstorage";
+  const storageAccount = process.env.storageName;
   const containerName: any = process.env.attachment;
-  const sasToken =
-    "sp=racwdl&st=2023-11-09T10:49:18Z&se=2023-12-30T18:49:18Z&sv=2022-11-02&sr=c&sig=%2FZvhReFGg0jpW6oX5BLY%2FzmiL86ocutLIbYmZkDsjH0%3D";
+  const sasToken = process.env.sasToken;
 
   const blobServiceClient = new BlobServiceClient(
     `https://${storageAccount}.blob.core.windows.net?${sasToken}`
